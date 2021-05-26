14fe96b980447e87e67a71d8ced22991725d4644
fengxin <fengxin@ushareit.com>
2021-05-26 16:32:44 +0800

    add test model

---
+++ spring-personal/src/main/java/com/lovejobs/ApplicationSimple.java
@@ -0,0 +1,18 @@
+package com.lovejobs;
+
+public class ApplicationSimple {
+
+	private String name;
+
+	public ApplicationSimple(String name) {
+		this.name = name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
---
+++ spring-personal/src/test/java/com/lovejobs/ApplicationSimpleTest.java
@@ -0,0 +1,23 @@
+package com.lovejobs;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Map;
+
+public class ApplicationSimpleTest {
+
+	@Test
+	public void testXml(){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationSimple applicationSimple = (ApplicationSimple) context.getBean("applicationSimple");
+		System.out.println(applicationSimple.getName());
+
+		JdbcTemplate template = (JdbcTemplate) context.getBean("jdbcTemplate");
+		Map<String, Object> map = template.queryForMap("select * from mcp_cdn_prov where id=1");
+		System.out.println(JSONObject.toJSONString(map));
+	}
+}
