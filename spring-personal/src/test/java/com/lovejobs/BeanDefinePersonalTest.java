64be1b99992c60bdde9bf4e3731bd14e90b63093
fengxin <fengxin@ushareit.com>
2021-05-27 17:26:22 +0800

    add test method

--- spring-personal/src/test/java/com/lovejobs/BeanDefinePersonalTest.java
+++ spring-personal/src/test/java/com/lovejobs/BeanDefinePersonalTest.java
@@ -2,9 +2,13 @@ package com.lovejobs;
 
 import org.junit.Test;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactoryTests;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.Resource;
 
 public class BeanDefinePersonalTest {
 
@@ -13,5 +17,18 @@ public class BeanDefinePersonalTest {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("beanFactoryPersonalTest.xml");
 		BeanDefinePersonal beanDefinePersonal = (BeanDefinePersonal) applicationContext.getBean("beanDefinePersonal");
 		System.out.println(beanDefinePersonal);
+	}
+
+	@Test
+	public void testBeanFactory(){
+		BeanFactory beanFactory = new ClassPathXmlApplicationContext("beanFactoryPersonalTest.xml");
+		BeanDefinePersonal beanDefinePersonal = (BeanDefinePersonal) beanFactory.getBean("beanDefinePersonal");
+		System.out.println(beanDefinePersonal);
+
+		DefaultListableBeanFactory beanFactory1 = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory1);
+		xmlBeanDefinitionReader.loadBeanDefinitions("beanFactoryPersonalTest.xml");
+		BeanDefinePersonal beanDefinePersonal1 = (BeanDefinePersonal) beanFactory1.getBean("beanDefinePersonal");
+		System.out.println(beanDefinePersonal1);
 	}
 }
