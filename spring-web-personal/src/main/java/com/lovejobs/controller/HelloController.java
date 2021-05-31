12bb728851b8bc918be7460d524b02df9222d254
fengxin <love_jobs@sina.cn>
2021-06-01 04:05:47 +0800

    add spring web test project

---
+++ spring-web-personal/src/main/java/com/lovejobs/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.lovejobs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	@RequestMapping("/string")
+	public String returnString() {
+		return "index";
+	}
+}
